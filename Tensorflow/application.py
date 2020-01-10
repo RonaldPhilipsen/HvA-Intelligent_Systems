5528280f092a80073898886cf64eaf3405a07b29
Ronald Philipsen <Mail@rphilipsen.nl>
2020-01-10 13:41:58 +0100

    minor fixes

--- Tensorflow/application.py
+++ Tensorflow/application.py
@@ -7,9 +7,13 @@ import numpy as np
 import copy, cv2, os
 
 
+camWidth, camHeight = 640, 480
+areaWidth, areaHeight = 300, 300
+
 data_folder = ".//data//"
 dataColor = (0,255,0)
 font = cv2.FONT_HERSHEY_SIMPLEX
+fontScale = 1.0
 fx, fy, fh = 10, 50, 45
 takingData = 0
 className = 'NONE'
@@ -38,15 +42,19 @@ def binaryMask(img):
 
 
 def main():
-    global font, size, fx, fy, fh
+    global font, fontScale, size, fx, fy, fh
     global takingData, dataColor
+    global camWidth, camHeight
+    global areaWidth, areaHeight
     global className, count
     global showMask
 
-    model = load_model('model_6cat.h5')
-    x0, y0, width = 200, 220, 300
+    model = load_model('best_model.h5')
+    x0, y0 = int((camWidth-areaWidth)/2), int((camHeight-areaHeight)/2)
 
-    cam = cv2.VideoCapture(0)
+    cam = cv2.VideoCapture(1)
+    cam.set(3,camWidth);
+    cam.set(4,camHeight);
     cv2.namedWindow('Original', cv2.WINDOW_NORMAL)
 
     while True:
@@ -54,24 +62,25 @@ def main():
         ret, frame = cam.read()
         frame = cv2.flip(frame, 1) # mirror
         window = copy.deepcopy(frame)
-        cv2.rectangle(window, (x0,y0), (x0+width-1,y0+width-1), dataColor, 12)
+        
+        cv2.rectangle(window, (x0,y0), ((x0+areaWidth-1), (y0+areaHeight-1)), dataColor, 12)
 
         # draw text
         if takingData:
             dataColor = (0,250,0)
-            cv2.putText(window, 'Data Taking: ON', (fx,fy), font, 1.2, dataColor, 2, 1)
+            cv2.putText(window, 'Data Taking: ON', (fx,fy), font, fontScale, dataColor, 2, 1)
+            cv2.putText(window, 'Class Name: %s (%d)' % (className, count), (fx,fy+fh), font, fontScale, (245,210,65), 2, 1)
         else:
             dataColor = (0,0,250)
-            cv2.putText(window, 'Data Taking: OFF', (fx,fy), font, 1.2, dataColor, 2, 1)
-        cv2.putText(window, 'Class Name: %s (%d)' % (className, count), (fx,fy+fh), font, 1.0, (245,210,65), 2, 1)
+            cv2.putText(window, 'Data Taking: OFF', (fx,fy), font, fontScale, dataColor, 2, 1)
 
         # get region of interest
-        roi = frame[y0:y0+width,x0:x0+width]
+        roi = frame[y0:y0+areaHeight,x0:x0+areaWidth]
         roi = binaryMask(roi)
 
         # apply processed roi in frame
         if showMask:
-            window[y0:y0+width,x0:x0+width] = cv2.cvtColor(roi, cv2.COLOR_GRAY2BGR)
+            window[y0:y0+areaHeight,x0:x0+areaWidth] = cv2.cvtColor(roi, cv2.COLOR_GRAY2BGR)
 
         # take data or apply predictions on ROI
         if takingData:
@@ -83,9 +92,7 @@ def main():
             img = np.expand_dims(img, axis=-1)
             pred = classes[np.argmax(model.predict(img)[0])]
             cv2.putText(window, 'Prediction: %s' % (pred), (fx,fy+2*fh), font, 1.0, (245,210,65), 2, 1)
-            # use below for demoing purposes
-            #cv2.putText(window, 'Prediction: %s' % (pred), (x0,y0-25), font, 1.0, (255,0,0), 2, 1)
-
+           
         # show the window
         cv2.imshow('Original', window)
 
@@ -99,7 +106,6 @@ def main():
         # Toggle data taking
         elif key == ord('s'):
             takingData = not takingData
-
         elif key == ord('b'):
             showMask = not showMask
 
@@ -112,24 +118,8 @@ def main():
         elif key == ord('4'):  initClass('FOUR')
         elif key == ord('5'):  initClass('FIVE')
 
-        # adjust the size of window
-        #elif key == ord('z'):
-        #    width = width - 5
-        #elif key == ord('a'):
-        #    width = width + 5
-
-        # adjust the position of window
-        elif key == ord('i'):
-            y0 = max((y0 - 5, 0))
-        elif key == ord('k'):
-            y0 = min((y0 + 5, window.shape[0]-width))
-        elif key == ord('j'):
-            x0 = max((x0 - 5, 0))
-        elif key == ord('l'):
-            x0 = min((x0 + 5, window.shape[1]-width))
-
     cam.release()
-
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     initClass('NONE')
